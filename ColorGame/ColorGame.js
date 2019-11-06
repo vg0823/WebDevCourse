1fc2c74ca9bc0021ef1e3b60882d5c8a9b1a0556
vidhyadhari <vid.gondle@gmail.com>
2019-11-05 23:13:58 -0500

    some more changes to include random colors

--- ColorGame/ColorGame.js
+++ ColorGame/ColorGame.js
@@ -1,26 +1,50 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-]
-
+var colors = generateRandomColors(6);
+console.log(colors);
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
+var messageDisplay = document.querySelector("#message");
 
 for( var i = 0; i<squares.length;i++){
     squares[i].style.backgroundColor = colors[i];
     squares[i].addEventListener("click", function(){
         clickedColor = this.style.backgroundColor;
         console.log(clickedColor);
-        if(clickedColor==pickedColor){
-            console.log("Picked correct color");
+        console.log(pickedColor);
+        if( clickedColor === pickedColor ){
+            //messageDisplay.textContent = "Correct";
+            changeColors( pickedColor );
+        } else{
+            //messageDisplay.textContent = "Wrong choice! Try again.";
+            this.style.backgroundColor = "#232323";
         }
     });
 }
 
+function changeColors( pickedColor ){
+    for ( var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = pickedColor;
+    }
+}
+
+function pickColor( ){
+    var rannum = Math.floor(Math.random()*6 + 1);
+    return colors[rannum];
+}
+
+function generateRandomColors(num){
+    arr = [];
+    for( var i=0; i < num; i++){
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random()*255 + 1);
+    var g = Math.floor(Math.random()*255 + 1);
+    var b = Math.floor(Math.random()*255 + 1);
+    return "rgb(" + r + ", " + g + ", "+ b +")";
+}
 
