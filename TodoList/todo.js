f64759afe5adaeafc28afca6a2b52fb6e011b4dc
vg0823 <49250893+vg0823@users.noreply.github.com>
2019-11-07 23:16:13 -0500

    Add files via upload

---
+++ TodoList/todo.js
@@ -0,0 +1,22 @@
+//Check off specific Todo by clicking
+$("ul").on("click", "li" ,function(){
+    $(this).toggleClass("completed");
+});
+
+//Click on X to delete todo
+$("ul").on("click","span", function(evt){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    evt.stopPropagation();
+});
+
+// Add item to the ToDo List
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        var todoText = $(this).val();
+        var liString = '<li><span>X</span> ' + todoText +'</li>';
+        $("ul").append( liString );
+        $(this).val("");
+    }
+});
