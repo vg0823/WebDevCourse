15e33c7d6f9c2eca292c672951d7a7d3d619e83b
vg0823 <vidhya.damamkar@gmail.com>
2019-11-24 21:57:53 -0500

    Additional changes for styling

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,20 +1,36 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 app.use(express.static('./public'));
+
+var campgrounds = [
+    {"name":"Salmon Creek", "image": "images/1149402.png"},
+    {"name":"Granite Hill", "image":"images/1846142.png"},
+    {"name":"Mountain Goat's Rest", "image":"images/1867275.png"}
+];
+
 app.get("/", function(req,res){
     res.send("landing");
 });
 
 app.get('/campgrounds', function(req,res){
-    var campgrounds = [
-        {"name":"Salmon Creek", "image": "images/1149402.png"},
-        {"name":"Granite Hill", "image":"images/1846142.png"},
-        {"name":"Mountain Goat's Rest", "image":"images/1867275.png"}
-    ];
-
     res.render("campground",{"campgrounds":campgrounds});
+});
+
+app.post('/campgrounds', function(req,res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name:name, image:image}
+    campgrounds.push(newCampground);
+
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
 });
 
 app.listen(3000, function(){
