51643359a4f1b256d540cf53374a521f84d6cd55
vg0823 <vidhya.damamkar@gmail.com>
2019-11-25 23:24:21 -0500

    Adding mongoose

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
@@ -12,19 +13,46 @@ var campgrounds = [
     {"name":"Mountain Goat's Rest", "image":"images/1867275.png"}
 ];
 
+campgrounds.create(
+    {
+        name: "Granite Hill",
+        image:"images/1846142.png",
+        description:"This is a huge granite hill"
+    }, function(err, campground){
+        if(err){
+            console.log(err);
+        }  else {
+            console.log("NEWLY CREATED CAMPGROUND");
+            console.log(campground);
+        }
+    }
+);
+
 app.get("/", function(req,res){
     res.send("landing");
 });
 
 app.get('/campgrounds', function(req,res){
-    res.render("campground",{"campgrounds":campgrounds});
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        }   else{
+            res.render("campground",{"campgrounds":allCampgrounds});
+        }
+    });
 });
 
 app.post('/campgrounds', function(req,res){
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name:name, image:image}
-    campgrounds.push(newCampground);
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }   else{
+
+        }
+    });
 
     res.redirect("/campgrounds");
 });
