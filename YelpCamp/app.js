e32ac2bb90011a640a43f541e9bf2a875d44f317
vg0823 <49250893+vg0823@users.noreply.github.com>
2019-12-22 23:07:34 -0500

    Changes related to comments

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
 
@@ -57,9 +58,34 @@ app.get("/campgrounds/:id", function(req,res){
 // comments routes
 //===============
 app.get("/campgrounds/:id/comments/new", function(req,res){
-    res.render("comments/new");
+    Campground.findById(req.params.id , function(err, campground){
+        if(err){
+            console.log(err);
+        }  else  {
+            res.render("comments/new", {campground: campground});
+        }
+    });
 });
 
+app.post("/campgrounds/:id/comments", function(req,res){
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+            res.redirect("/campgrounds");
+        }  else  {
+            Comment.create(req.body.comment, function(err, comment){
+                if(err){
+                    console.log(err);
+                    res.redirect("/campgrounds");
+                }  else  {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect('/campgrounds/'+campground._id);
+                }
+            });
+        }
+    })
+});
 app.listen(3000, function(){
     console.log("The YelpCamp server has started!");
 });
