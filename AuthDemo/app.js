09dd728a492b91010fff103379ecfee8712c8e09
vg0823 <49250893+vg0823@users.noreply.github.com>
2020-01-13 00:17:02 -0500

    Authentication using Passport library.

---
+++ AuthDemo/app.js
@@ -0,0 +1,72 @@
+var express               = require("express"),
+    mongoose              = require("mongoose"),
+    passport              = require("passport"),
+    bodyParser            = require("body-parser"),
+    User                  = require("./models/user"),
+    LocalStrategy         = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://localhost/auth_demo_app");
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("express-session")({
+    secret: "Rusty is the best and cutest dog in the world.",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//=======
+// ROUTES
+//=======
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.get("/secret", function(req,res){
+    res.render("secret");
+});
+
+// Auth Routes
+
+//show sign up form
+
+app.get("/register", function(req,res){
+    res.render("register");
+}); 
+
+app.post("/register", function(req,res){
+    User.register(new User({username:req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render('register');
+        }
+        passport.authenticate("local")(req,res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+// Login Routes
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect:"/secret",
+    failureRequest:"/login"
+}), function(req,res){
+
+});
+
+app.listen(3000, function(){
+    ;console.log("server started");
+});
