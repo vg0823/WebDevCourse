e76ab18c38a1bb713c4c8cf6c7159f4f935ee11a
vg0823 <49250893+vg0823@users.noreply.github.com>
2020-01-13 00:28:18 -0500

    Adding Logout page to the AuthDemo

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -31,7 +31,7 @@ app.get("/", function(req,res){
     res.render("home");
 });
 
-app.get("/secret", function(req,res){
+app.get("/secret", isLoggedIn, function(req,res){
     res.render("secret");
 });
 
@@ -64,8 +64,19 @@ app.post("/login", passport.authenticate("local", {
     successRedirect:"/secret",
     failureRequest:"/login"
 }), function(req,res){
-
 });
+
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 app.listen(3000, function(){
     ;console.log("server started");
